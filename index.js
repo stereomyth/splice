c77bc6b363acfee050ef38d5d16294b995cb528b
James King <j@lostwith.me>
2018-05-27 11:39:02 +0100

    duplicate original lambda

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const axios = require('axios');
+const parse = require('./steps/parse.js');
+const convert = require('./steps/convert.js');
+const save = require('./steps/save.js');
+
+const url =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3333/weekly_film_times.xml'
+    : 'https://www.cineworld.co.uk/syndication/weekly_film_times.xml';
+
 module.exports = (req, res) => {
-  res.end('Welcome to Micro');
+  axios
+    .get(url)
+    .then(res => parse(res.data))
+    .then(convert)
+    .then(save)
+    .then(data => {
+      res.end(JSON.stringify(data));
+    })
+    .catch(err => {
+      console.log(err);
+      res.end('nope');
+    });
 };
